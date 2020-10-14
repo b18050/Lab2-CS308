4bb93e0ef477dd9ce33e7616fc75952ba73dabd2
chandan <abchandan11@gmail.com>
2020-10-14 21:28:42 +0530

    Added conflict prefix NEGSUPPORT

--- PartA/sqrt.c
+++ PartA/sqrt.c
@@ -18,11 +18,11 @@ int main(int argc, char* argv[]) {
 	if(input < 0){
 		double complex num = input + 0.0;
 		double complex s = csqrt(num);
-		printf("%.2f + %.2fi\n", creal(s), cimag(s));
+		printf("NEGSUPPORT: %.2f + %.2fi\n", creal(s), cimag(s));
 	}
 	else {
-		printf("Sqrt of %d is %f\n",input,sqrt(input));
-		printf("End of program. Exiting.");
+		printf("NEGSUPPORT: Sqrt of %d is %f\n",input,sqrt(input));
+		printf("NEGSUPPORT: End of program. Exiting.");
 	}
 	return(0);
 
